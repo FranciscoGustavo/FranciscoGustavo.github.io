133019df25fac7173b3439aee7360d6ea471e796
FranciscoGustavo <hidalgo_fco520@outlook.com>
2016-10-12 11:59:51 -0500

    BibliotecaGeek in FranciscoGustavo.github.io
    Add the page od bibliotecageek in github pages
    (FranciscoGustavo.github.io)

--- js/main.js
+++ js/main.js
@@ -1,48 +1,19 @@
-var menu = document.getElementById('nav');
-var activar = 0;
+var menu = document.getElementById('menu');
+var backgroundDark = document.getElementById('background-dark');
+var active = 0;
 
-function openMenu () {
-	if (activar == 0) {
-		menu.style.padding = "10px 0";
-		menu.style.height = "auto";
-		menu.style.transition = "all 0.1s";
-		
-		activar = 1;
-	} else {
-		menu.style.padding = "0";
-		menu.style.height = "0";
-		activar = 0;
-	}
+function switchMenu () {
+  if (active == 0) {
+    stylesMenu(0,1);
+    active = 1;
+  } else {
+    stylesMenu(100,0);
+    active = 0;
+  }
 }
 
+function stylesMenu(traslateX, scale) {
+  menu.style.transform = "translateX(-" + traslateX + "%)";
+  backgroundDark.style.transform = "scale(" + scale + ")";
 
-var user = document.getElementById('user');
-var sombra = document.getElementById('background');
-var body = document.getElementById('body');
-
-function openUser() {
-	body.style.overflowY = "hidden";
-	user.style.transform = "translateX(0%)";
-	user.style.transition = "all 0.1s";
-	sombra.style.transition = "all 0.1s";
-	sombra.style.height = "100vh";
-	sombra.style.transition = "all 0.1s";
 }
-
-function closeUser() {
-	body.style.overflowY = "scroll";
-	user.style.transform = "translateX(-100%)";
-	sombra.style.height = "0";
-	sombra.style.transition = "all 0.1s";
-}
-
-var nav = document.getElementById('menu');
-var lis = document.getElementById('lista');
-var total = 0;
-function autoMenu() {
-	total = nav.offsetWidth - 620;
-	lis.style.marginLeft = total + "px";
-	//alert(total);
-}
-
-autoMenu();
