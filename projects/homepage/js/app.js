dfe7868ba27ea40375186fd49ec98718d1f43938
FranciscoGustavo <hidalgo_fco520@outlook.com>
2017-02-17 21:56:45 -0600

    Agregando el cambio de formularios

--- projects/homepage/js/app.js
+++ projects/homepage/js/app.js
@@ -10,19 +10,37 @@
 				element  = e.target;
 			}
 
-			if(element.tagName == "BUTTON"){			
-				element.addEventListener("click",function(){
-					if(element.querySelector('.ripple')){
-						addEfectRipple(element)
-					} else{
-						setTimeout(function(){
-							element.blur()
-						},400);
-					}
-				})
+			if(element.className == "signUp inactive"){
+				console.log("HOLA")
+
+				formulario(sign_up,sign_in)
+				signIn.className = "signIn inactive"
+				signUp.className = "signUp"
+
+
+				
+			} else if(element.className == "signIn inactive"){
+				formulario(sign_in,sign_up)
+				signIn.className = "signIn"
+				signUp.className = "signUp inactive"
 			}
 
 		})
+
+		sign_up = document.querySelector('.sign_up');
+		sign_in = document.querySelector('.sign_in');
+		signUp = document.querySelector('.signUp');
+		signIn = document.querySelector('.signIn');
+
+		function formulario (elementShow, elementHide){
+			elementShow.style.width = "100%";
+			elementShow.style.padding = "20px";
+			elementShow.className = "sign_up"
+
+			elementHide.style.width = "0";
+			elementHide.style.padding = "0";
+			elementHide.style.overflow = "hidden";
+		}
 
 	})
 }())
