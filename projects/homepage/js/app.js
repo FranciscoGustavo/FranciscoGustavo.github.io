470094ea8a212928caad5382dccea42e190962c1
FranciscoGustavo <hidalgo_fco520@outlook.com>
2017-02-20 20:47:05 -0600

    Arreglamos el diseño y el codigo

--- projects/homepage/js/app.js
+++ projects/homepage/js/app.js
@@ -1,37 +1,65 @@
+(function detectarNavegador(navegador) {
+	html = document.getElementsByTagName("html")[0]
+    if(navegador.indexOf("Chrome") != -1 ) {
+    	html.className = html.className + " chrome"
+    }
+    else if(navegador.indexOf("Opera") != -1 ) {
+    	html.className = html.className + " opera"
+    }
+    else if(navegador.indexOf("Firefox") != -1 ) {
+    	html.className = html.className + " firefox"
+    }
+    else if((navegador.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {
+    	html.className = html.className + " ie"
+    }
+    else if(navegador.indexOf("Safari") != -1 ) {
+    	html.className = html.className + " safari"
+    }  
+    else {
+        html.className = html.className + " other"
+    }
+})(navigator.userAgent);
+
 (function (){
+
+	//Evento principal (espera a que el codigo HTML termine de cargar)
 	window.addEventListener("load",function(){
 
+		// variables
 		var body = document.getElementsByTagName('body')[0];
+		form =  document.sign_in;
+		elementos = form.elements;
 
+		// eventos
 		body.addEventListener("mousedown", function(e){
 			if (e.srcElement){
 				element  = e.srcElement;
 			}else if (e.target){
 				element  = e.target;
 			}
-
-			if(element.className == "signUp inactive"){
-				console.log("HOLA")
-
-				formulario(sign_up,sign_in)
-				signIn.className = "signIn inactive"
-				signUp.className = "signUp"
-
-
-				
-			} else if(element.className == "signIn inactive"){
-				formulario(sign_in,sign_up)
-				signIn.className = "signIn"
-				signUp.className = "signUp inactive"
-			}
-
 		})
 
-		sign_up = document.querySelector('.sign_up');
-		sign_in = document.querySelector('.sign_in');
-		signUp = document.querySelector('.signUp');
-		signIn = document.querySelector('.signIn');
+		for (var i = 0;  i < elementos.length; i++) {
+			if(elementos[i].type == "text" || elementos[i].type == "email" || elementos[i].type == "password"){
+				elementos[i].addEventListener("focus",function(){
+					console.log('Hola')
+					console.log(this.parentElement.children[1].className)
+					this.parentElement.children[1].className = this.parentElement.children[1].className + " active-label"
+				})
 
+				elementos[i].addEventListener("blur",function(){
+					console.log('Hola 2')
+					console.log(this.parentElement.children[1].className)
+					this.parentElement.children[1].className = this.parentElement.children[1].className.replace("active-label", "")
+				})
+			}
+
+			console.log('Dentro del bucle')
+		}
+
+
+
+		// funciones
 		function formulario (elementShow, elementHide){
 			elementShow.style.width = "100%";
 			elementShow.style.padding = "20px";
