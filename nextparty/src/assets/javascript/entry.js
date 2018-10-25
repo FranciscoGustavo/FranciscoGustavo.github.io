b99832a10bda5408c6dc87b9e9f16e0588bae6d5
Francisco <hidalgo_fco520@outlook.com>
2018-10-25 16:20:08 -0500

    Add Details

--- nextparty/src/assets/javascript/entry.js
+++ nextparty/src/assets/javascript/entry.js
@@ -13,11 +13,7 @@ import '../css/app.scss';
     let barsIcon = 'fas fa-bars';
     barsIcon == icon.className ? icon.className = 'fas fa-times' : icon.className = barsIcon;
   }
-
-  let query = 'nav a[href=".' + window.location.pathname + '"]';
-
-  let item = menu.querySelector(query);
-  item.classList.add('active');
+  
   
   let form = document.querySelector('#formContact');
   if(form) {
@@ -28,6 +24,29 @@ import '../css/app.scss';
       formContainer.classList.add('sended');
       formContainer.innerHTML = "<h1>Mensaje enviado</h1>"
     });
+  }
+
+  let options = [
+    { page: 'index', item: 1 },
+    { page: 'events', item: 2 },
+    { page: 'grantour', item: 3 },
+    { page: 'contact', item: 4 }
+  ];
+
+  
+
+  let page = document.querySelector('body').getAttribute('data-page');
+
+  options.forEach(function(option){
+    option.page == page ? activeOptionMenu(option.item) : console.log("no es");
+  });
+
+  
+
+  
+
+  function activeOptionMenu(element){
+    document.querySelector('nav a:nth-child(' + element + ')').classList.add('active');
   }
 
 })();
